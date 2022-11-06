6495b079bc6ac4f21376e64ccf3b1689fe114d62
MSI\MSI Modern 14 <amp0203>
2022-11-06 18:45:39 +0700

    new to js

---
+++ test.js
@@ -0,0 +1,18 @@
+console.log('Cu Thi Phuong');
+let message;
+
+message = 'Hello'; // store the string 'Hello' in the variable named message
+
+let m2 = ' world!';
+console.log(message + m2);
+
+// message = false; // falsy: 0 '' null undefined
+
+if (message) {
+  console.log('aaaaaaa');
+}
+
+// let user = 'John', age = 25, message1 = 'Hello';
+let user = 'John';
+let age = 25;
+let message = 'Hello';
